43a57f0df52540b8d7c17daac14969104d800314
plokm1234 <wyuenab@gmail.com>
2019-12-30 17:05:28 +0800

    Updates

---
+++ precache-manifest.bc2b896c35166f9420dab6acdbdc824b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f309d085d40137f2c574c13b67940cee",
+    "url": "/RandomQuoteMachine/index.html"
+  },
+  {
+    "revision": "5e943f320a98e10c0d97",
+    "url": "/RandomQuoteMachine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "676bb4b1a05e436b425e",
+    "url": "/RandomQuoteMachine/static/js/2.6187bb79.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/RandomQuoteMachine/static/js/2.6187bb79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e943f320a98e10c0d97",
+    "url": "/RandomQuoteMachine/static/js/main.bfb4b5d1.chunk.js"
+  },
+  {
+    "revision": "7aa487680fd88b8357af",
+    "url": "/RandomQuoteMachine/static/js/runtime-main.19e009cd.js"
+  }
+]);
